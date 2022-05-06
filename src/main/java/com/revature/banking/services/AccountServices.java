aa4709d2c8c43af1964e9fe286bf2471ab866c41
HungFai Koo <jhk888@yahoo.com>
2022-05-06 11:59:55 -0700

    services folder and  models account updated

--- src/main/java/com/revature/banking/services/AccountServices.java
+++ src/main/java/com/revature/banking/services/AccountServices.java
@@ -1,7 +1,14 @@
 package com.revature.banking.services;
 
 import com.revature.banking.daos.NewUserDao;
+import com.revature.banking.exceptions.InvalidRequestException;
+import com.revature.banking.exceptions.ResourcePersistanceException;
 import com.revature.banking.models.Account;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import java.io.IOException;
 
@@ -9,13 +16,13 @@ public class AccountServices {
 
     private NewUserDao newUserDao = new NewUserDao();
 
-    public void readTrainers(){
-        System.out.println("Begin reading Trainers in our file database.");
-        user[] newUsers;
+    public void readAccounts(){
+        System.out.println("Begin reading Account in our file database.");
+        Account[] accounts;
 
         try {
             // TODO: What trainerDao intellisense telling me?
-            Account = NewUserDao.findAll();
+            accounts = NewUserDao.findAll();
             System.out.println("All newUsers have been found here are the results: \n");
 //            for (int i = 0; i < trainers.length; i++) {
 //                Trainer trainer = trainers[i];
@@ -28,24 +35,24 @@ public class AccountServices {
             // trainer is now declared as a reference variables for an instance of a Trainer class
             // new keyword allows for the construction (or more technically the instantiation of a Trainer class with a No-Arg Construtor)
             // new Trainer() is instantiating a new trainer object via the No-Args Constructor
-             Account = new NewUser();
+             Account account = new Account();
 
             // TODO: Why is this declared as an Object and not a Trainer??
-            Object user1 = new User("Charles", "Jester", "cj@mail.com", "p", "1111");
+            Object account1 = new Account("Charles", "Jester", "cj@mail.com", "p", "1111");
 
-            NewUser iCanNameThisWhatEverTheHeckoIWant = new Account();
+            Account iCanNameThisWhatEverTheHeckoIWant = new Account();
             System.out.println(iCanNameThisWhatEverTheHeckoIWant.getEmail());
 
             System.out.println(" ----------THIS THINGGGGGGGG--------------- ");
-            System.out.println(user1.toString());
+            System.out.println(account1.toString());
             System.out.println("-------------------------");
             // the (User) is casting the Object trainer1 in java's Heap Memory to view as a Trainer object instead
-            System.out.println(((User) User1).getEmail());
+            System.out.println(((Account) account1).getEmail());
 
             // forEach
-            for(Object t:Users ){
-                if(t != null) {
-                    System.out.println((Account) t); // trainer indicates a single element in the trainers array
+            for(Object a:accounts ){
+                if(a != null) {
+                    System.out.println((Account) a); // trainer indicates a single element in the trainers array
                 }
             }
 
@@ -66,33 +73,33 @@ public class AccountServices {
         return false;
     }
 
-    public boolean registerUser(User newUser){
-        System.out.println("Trainer trying to be registered: " + newUser);
-        if(!validateUserInput(newUser)){ // checking if false
+    public boolean registerAccount(Account newAccount){
+        System.out.println("Account trying to be registered: " + newAccount);
+        if(!validateUserInput(newAccount)){ // checking if false
             System.out.println("User was not validated");
             // TODO: throw - what's this keyword?
             throw new RuntimeException();
         }
 
         // TODO: Will implement with JDBC (connecting to our database)
-        validateEmailNotUsed(newUser.getEmail());
+        validateEmailNotUsed(newAccount.getEmail());
 
-        User persistedUser = NewUserDao.create(newUser);
+        Account persistedAccount = NewUserDao.create(newAccount);
 
-        if(persistedUser == null){
+        if(persistedAccount == null){
             throw new RuntimeException();
         }
-        System.out.println("User has been persisted: " + newUser);
+        System.out.println("User has been persisted: " + newAccount);
         return true;
     }
 
-    private boolean validateUserInput(User newUser) {
-        System.out.println("Validating User: " + newUser);
-        if(newUser == null) return false;
-        if(newUser.getEmail() == null || newUser.getEmail().trim().equals("")) return false;
-        if(newUser.getPassword() == null || newUser.getPassword().trim().equals("")) return false;
-        if(newUser.getFirst_Name() == null || newUser.getFirst_Name().trim().equals("")) return false;
-        if(newUser.getLast_Name() == null || newUser.getLast_Name().trim().equals("")) return false;
-        return newUser.getAge() != null || !newUser.getAge().trim().equals("");
+    private boolean validateUserInput(Account newAccount) {
+        System.out.println("Validating User: " + newAccount);
+        if(newAccount == null) return false;
+        if(newAccount.getEmail() == null || newAccount.getEmail().trim().equals("")) return false;
+        if(newAccount.getPassword() == null || newAccount.getPassword().trim().equals("")) return false;
+        if(newAccount.getFirst_Name() == null || newAccount.getFirst_Name().trim().equals("")) return false;
+        if(newAccount.getLast_Name() == null || newAccount.getLast_Name().trim().equals("")) return false;
+        return newAccount.getAge() != null || !newAccount.getAge().trim().equals("");
     }
 }
